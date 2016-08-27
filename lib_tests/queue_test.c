53835bb61781e65353d5392ad29273350ef58963
Bobby Crawford (ozdevguy) <ozdevguy@gmail.com>
2016-08-26 22:46:42 -0400

    changes...

--- lib_tests/queue_test.c
+++ lib_tests/queue_test.c
@@ -1,10 +1,30 @@
 #include "../lib/queue.h"
+#include "../lib/utf8.h"
+#include "../lib/string.h"
+
+void* logger(byte* data, size_t size, uint8_t type, uint32_t id){
+
+	printf("Data: %s\n", (char*)data);
+
+	return 0;
+
+}
+
+
 
 void main(){
 
 	int first = 61092;
 	int second = 1213;
 	int third = 3543;
+
+	_std_lib_environment_setup(3232);
+	_std_lib_environment_set_memory_allocator(0);
+	_std_lib_environment_set_memory_deallocator(0);
+	_std_lib_environment_set_logger(logger);
+
+	STD_LIB_STATIC_ENV.logger("hello there!\n", 1, 1, 1);
+
 
 	queue* myQueue = _queue_new(1);
 
