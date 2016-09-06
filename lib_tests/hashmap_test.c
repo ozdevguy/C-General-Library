9b57738d6678bcde47766e21a5e35106f99b7927
Bobby Crawford (ozdevguy) <ozdevguy@gmail.com>
2016-09-06 15:34:13 -0400

    Newest commit.

--- lib_tests/hashmap_test.c
+++ lib_tests/hashmap_test.c
@@ -2,7 +2,37 @@
 
 void main(){
 
-	hashmap* hm;
+	standard_library_context ctx;
+
+	_std_lib_default(&ctx);
+
+	int val1 = 1234;
+	int val2 = 5678;
+
+	map* myHashmap;
+
+	string* myNameString = _string_new(&ctx);
+	_string_append_fbytes(myNameString, "Value 1");
+
+	string* myNameString2 = _string_new(&ctx);
+	_string_append_fbytes(myNameString2, "Value 2");
+
+	myHashmap = _hashmap_new(&ctx, 10);
+	_hashmap_insert(myHashmap, myNameString, &val1, 1, 1);
+	_hashmap_insert(myHashmap, myNameString2, &val2, 1, 1);
+
+	map_entry entry;
+
+	entry = _hashmap_lookup(myHashmap, myNameString);
+
+	printf("Val: %d\n", *((int*)entry.data));
+
+	entry = _hashmap_lookup(myHashmap, myNameString2);
+
+	printf("Val: %d\n", *((int*)entry.data));
+
+	_string_delete(myNameString);
+	_string_delete(myNameString2);
 	
 	return;
 }
