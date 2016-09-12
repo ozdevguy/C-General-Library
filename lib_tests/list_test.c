9aca0fa7b651889648abbc5c6c9ab2c4780f7af4
Bobby Crawford (ozdevguy) <ozdevguy@gmail.com>
2016-09-12 14:22:28 -0400

    Finished list..

--- lib_tests/list_test.c
+++ lib_tests/list_test.c
@@ -6,17 +6,37 @@ void main(){
 
 	int a = 1234;
 	int b = 9843;
+	int d = 4343;
 
 	_std_lib_default(&ctx);
 
-	list* myList = _list_new(&ctx, 10);
+	list* myList = _list_new(&ctx, 2);
 
 	_list_add(myList, &a);
-	//_list_add(myList, &b);
+	_list_add(myList, &b);
+	_list_add(myList, &a);
+	_list_add(myList, &b);
+	_list_add(myList, &d);
 
-	printf("%p\n", _list_get(myList, 0));
+	_list_remove(myList, 3);
 
-	printf("%p\n", &a);
+	
+	_list_reset_iterator(myList);
 
+	while(_list_has_next(myList))
+		printf("%d\n", *((int*)_list_get_next(myList)));
+
+	_list_add(myList, &a);
+
+	printf("\n\n");
+
+	_list_reset_iterator(myList);
+
+	while(_list_has_next(myList))
+		printf("%d\n", *((int*)_list_get_next(myList)));
+
+	printf("\n\n%d", *((int*)_list_get(myList, _list_find(myList, &d, 0))));
+
+	_list_delete(myList);
 
 }
